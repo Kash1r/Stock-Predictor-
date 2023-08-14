e1598d925d0ad022fd4fcf3994cab30b87c07caa
Kashir Khan <98199546+Kash1r@users.noreply.github.com>
2023-08-14 01:45:11 -0500

    Update Stock_Predictor.py

--- Stock_Predictor.py
+++ Stock_Predictor.py
@@ -1,3 +1,4 @@
+# Necessary imports
 import numpy as np
 import pandas as pd
 import torch
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 
 # Define the LSTM model class
 class LSTM(nn.Module):
-    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
+    def __init__(self, input_size=20, hidden_layer_size=50, output_size=1):
         super().__init__()
         self.hidden_layer_size = hidden_layer_size
 
@@ -29,13 +30,11 @@ class LSTM(nn.Module):
         # Pass the output of the LSTM through the output layer
         predictions = self.linear(lstm_out.view(len(input_seq), -1))
 
-        # Return the predictions
-        return predictions[-1]
+        # Return all the predictions
+        return predictions
 
 # Load the historical stock price data
-# This data might be obtained from a financial API or a CSV file
-# For day trading, you would want to use high-frequency data, such as minute-by-minute price data
-data = pd.read_csv('stock_prices.csv')
+data = pd.read_csv(r"C:\Users\Kashir\Desktop\Side_Projects\TSLA.csv")  # Replace with the path to your file
 
 # We're only interested in the 'Close' column, so we'll extract that
 data = data['Close'].values
@@ -54,8 +53,7 @@ test_size = len(data) - train_size
 train, test = data[0:train_size,:], data[train_size:len(data),:]
 
 # Convert an array of values into a dataset matrix
-# This function takes a dataset and a 'look_back' parameter, which determines the number of previous time steps to use as input variables to predict the next time period
-def create_dataset(dataset, look_back=60):
+def create_dataset(dataset, look_back=20):
     dataX, dataY = [], []
     for i in range(len(dataset)-look_back-1):
         a = dataset[i:(i+look_back), 0]
@@ -63,25 +61,26 @@ def create_dataset(dataset, look_back=60):
         dataY.append(dataset[i + look_back, 0])
     return np.array(dataX), np.array(dataY)
 
+# Further reduce the 'look_back' parameter to 20
+look_back = 20
+
+# Reduce the size of the hidden layer in the LSTM model to 50
+hidden_layer_size = 50
+
 # Reshape the train and test data
-# We're using a 'look_back' of 60, which means we'll use the past 60 minutes of price data to predict the next minute
-# Look_back can be modified as needed.
-look_back = 60
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 
 # Convert the data to PyTorch tensors
-# PyTorch models require input data in the form of tensors
 trainX = torch.FloatTensor(trainX)
 trainY = torch.FloatTensor(trainY)
 testX = torch.FloatTensor(testX)
 testY = torch.FloatTensor(testY)
 
 # Create the LSTM model
-model = LSTM()
+model = LSTM(input_size=look_back, hidden_layer_size=hidden_layer_size)  # Adjust the input size and the hidden layer size of the model
 
 # Define the loss function and the optimizer
-# We're using Mean Squared Error (MSE) as the loss function and Adam as the optimizer
 loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
@@ -98,7 +97,7 @@ for i in range(100):
     y_pred = model(trainX)
 
     # Compute the loss
-    single_loss = loss_function(y_pred, trainY)
+    single_loss = loss_function(y_pred, trainY.view(-1, 1))  # Add view(-1, 1)
 
     # Backward pass
     single_loss.backward()
@@ -106,40 +105,34 @@ for i in range(100):
     # Update the weights
     optimizer.step()
 
-# Switch to evaluation mode
-model.eval()
-
 # Make predictions on the training data and the test data
 trainPredict = model(trainX)
 testPredict = model(testX)
 
 # Invert the predictions back to the original scale
-# We had normalized our dataset to the range [0, 1], but we want to compare the predictions to the original data
-trainPredict = scaler.inverse_transform(trainPredict.detach().numpy().reshape(-1, 1))
-trainY = scaler.inverse_transform(trainY.detach().numpy().reshape(-1, 1))
-testPredict = scaler.inverse_transform(testPredict.detach().numpy().reshape(-1, 1))
-testY = scaler.inverse_transform(testY.detach().numpy().reshape(-1, 1))
+trainPredict = scaler.inverse_transform(trainPredict.detach().numpy())
+trainY = scaler.inverse_transform(trainY.view(-1, 1).detach().numpy())  # Add view(-1, 1)
+testPredict = scaler.inverse_transform(testPredict.detach().numpy())
+testY = scaler.inverse_transform(testY.view(-1, 1).detach().numpy())  # Add view(-1, 1)
 
-# Calculate the root mean squared error (RMSE) of the predictions
-# This will give us a single number that summarizes how close the predicted values are to the actual values
-trainScore = np.sqrt(mean_squared_error(trainY[:,0], trainPredict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-testScore = np.sqrt(mean_squared_error(testY[:,0], testPredict[:,0]))
-print('Test Score: %.2f RMSE' % (testScore))
+# Create a time axis for the training and testing data
+trainTime = np.arange(look_back, len(trainY) + look_back)
+testTime = np.arange(look_back + len(trainY), len(data) - 1)
 
-# Shift train predictions for plotting
-# We must shift the predictions so that they align on the x-axis with the original dataset
-trainPredictPlot = np.empty_like(data)
-trainPredictPlot[:, :] = np.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# Plot the actual data
+plt.figure(figsize=(15,7))
+plt.plot(np.append(trainY, testY), label='Actual')
 
-# Shift test predictions for plotting
-testPredictPlot = np.empty_like(data)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(data)-1, :] = testPredict
+# Create a time axis for the training and testing data
+testTime = np.arange(len(trainY) + look_back, len(trainY) + len(testPredict) + look_back)
 
-# Plot the original dataset, the predictions on the training dataset, and the predictions on the test dataset
-plt.plot(scaler.inverse_transform(data))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
+# Plot the predictions for the training and testing data
+plt.plot(trainTime, trainPredict, label='Train Predict')
+plt.plot(testTime, testPredict, label='Test Predict')
+
+plt.title('Stock Price Prediction')
+plt.xlabel('Time Step')
+plt.ylabel('Normalized Price')
+plt.legend()
+plt.grid(True)
 plt.show()
